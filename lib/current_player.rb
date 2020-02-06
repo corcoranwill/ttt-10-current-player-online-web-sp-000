ac6dfcaccc9c71b3df86f8f737d1365c0b930189
Will Corcoran <corcoranwill@gmail.com>
2020-02-06 01:52:29 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  # counts occupied positions
+  # loop thru board and determine how many spaces have been filled with X or O
+
+end
+
+def current_player()
+  # returns the correct player, X, for the first move
+  # returns the correct player, O, for the second move
+  # returns the correct player, X, for the third move
+
+end

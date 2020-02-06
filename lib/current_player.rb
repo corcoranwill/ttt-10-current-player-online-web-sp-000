c613c420597ff2475a1e4ae48e840aa90b2b2fc6
Will Corcoran <corcoranwill@gmail.com>
2020-02-06 02:21:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
-  board.each do |index|
-    if board|index| == "X"
+  board.each do |turn|
+    if board|turn| == "X"
       counter += 1
-    elsif board|index| == "O"
+    elsif board|turn| == "O"
       counter += 1
     end
   end
@@ -13,9 +13,9 @@ end
 
 def current_player(board)
   turn_count(board)
-  if turn_count % 2 == 1
+  if turn_count % 2 == 0
     puts "player X"
-  elsif turn_count % 2 == 0
+  elsif turn_count % 2 == 1
     puts "player O"
   end
 end

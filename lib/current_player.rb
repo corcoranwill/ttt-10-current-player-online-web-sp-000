120a8af5f954eb8c8a05c15e70292cc868a0fbd9
Will Corcoran <corcoranwill@gmail.com>
2020-02-06 02:16:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,9 +13,9 @@ end
 
 def current_player(board)
   turn_count(board)
-  if turn_count % 2 == 0
+  if turn_count % 2 == 1
     puts "player X"
-  elsif turn_count % 2 == 1
+  elsif turn_count % 2 == 0
     puts "player O"
   end
 end

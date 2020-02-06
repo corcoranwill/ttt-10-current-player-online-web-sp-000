217ee45fabac511937f029b3badae56d2f280c39
Will Corcoran <corcoranwill@gmail.com>
2020-02-06 02:35:32 +0000

    Automatically backed up by Learn

--- spec/current_player_spec.rb
+++ spec/current_player_spec.rb
@@ -14,20 +14,20 @@ describe "./lib/current_player.rb" do
   # describe '#current_player' do
   #   it 'returns the correct player, X, for the first move' do
   #     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  #
+  # 
   #     expect(current_player(board)).to eq("X")
   #   end
-  #
+  # 
   #   it 'returns the correct player, O, for the second move' do
   #     board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-  #
+  # 
   #     expect(current_player(board)).to eq("O")
   #   end
-  #
+  # 
   #   it 'returns the correct player, X, for the third move' do
   #     board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-  #
+  # 
   #     expect(current_player(board)).to eq("X")
   #   end
-  end
+  # end
 end
